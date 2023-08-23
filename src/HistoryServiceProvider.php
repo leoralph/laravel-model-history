d9d40994c96935a8bd08d4d68d5b2b9ace4f02f9
Leonardo Ralph <theleoralph@gmail.com>
2023-08-22 21:37:02 -0300

    fix: provider publish adjustments

--- src/HistoryServiceProvider.php
+++ src/HistoryServiceProvider.php
@@ -8,8 +8,14 @@ class HistoryServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $timestamp = date('Y_m_d_His', time());
+
         $this->publishes([
             __DIR__.'/../config/model-history.php' => config_path('model-history.php'),
         ], 'config');
+
+        $this->publishes([
+            __DIR__ . "/database/migrations" => database_path("migrations"),
+        ], 'migrations');
     }
 }
